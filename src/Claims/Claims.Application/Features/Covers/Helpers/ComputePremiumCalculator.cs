beaf2dd27aec50ee5f8669ab6e3ebefc7702e3c9
MustafaUrhan <hacimu@gmail.com>
2024-10-02 11:49:27 +0300

    Task 5 checked and completed

--- src/Claims/Claims.Application/Features/Covers/Helpers/ComputePremiumCalculator.cs
+++ src/Claims/Claims.Application/Features/Covers/Helpers/ComputePremiumCalculator.cs
@@ -14,7 +14,7 @@ public static class ComputePremiumCalculator
         {
             if (i < 30) totalPremium += premiumPerDay;
             if (i < 180) totalPremium += premiumPerDay - ((coverType != null && coverType == CoverType.Yacht) ? premiumPerDay * 0.05m : premiumPerDay * 0.02m);
-            if (i < 365) totalPremium += premiumPerDay - ((coverType != null && coverType == CoverType.Yacht) ? premiumPerDay * 0.08m : premiumPerDay * 0.03m);
+            if (i < 365) totalPremium += premiumPerDay - ((coverType != null && coverType == CoverType.Yacht) ? premiumPerDay * 0.03m : premiumPerDay * 0.01m);
         }
 
         return totalPremium;
--- src/Claims/Claims.Domain/Statics/CoverTypePremiums.cs
+++ src/Claims/Claims.Domain/Statics/CoverTypePremiums.cs
@@ -4,19 +4,19 @@ namespace Claims.Domain.Statics;
 
 public static class CoverTypePremiums
 {
-    public static decimal GetPremium(CoverType? coverType=null)
+    public static decimal GetPremium(CoverType? coverType = null)
     {
         if (coverType == null)
         {
-            return 1.3m;
+            return 1.3m + (1.3m * 0.3m);
         }
 
         return coverType switch
         {
-            CoverType.Yacht => 1.1m,
-            CoverType.PassengerShip => 1.2m,
-            CoverType.Tanker => 1.5m,
-            _ => 1.3m
+            CoverType.Yacht => 1.1m + (1.1m * 0.1m),
+            CoverType.PassengerShip => 1.2m + (1.2m * 0.2m),
+            CoverType.Tanker => 1.5m + (1.5m * 0.5m),
+            _ => 1.3m + (1.3m * 0.3m),
         };
     }
 }
