41b9f0e73a3da1513a450cc2466dd04e8dec4dac
MustafaUrhan <hacimu@gmail.com>
2024-10-02 11:55:14 +0300

    Task 5 check 2 and completed

--- src/Claims/Claims.Application/Features/Covers/Helpers/ComputePremiumCalculator.cs
+++ src/Claims/Claims.Application/Features/Covers/Helpers/ComputePremiumCalculator.cs
@@ -13,8 +13,8 @@ public static class ComputePremiumCalculator
         for (var i = 0; i < insuranceLength; i++)
         {
             if (i < 30) totalPremium += premiumPerDay;
-            if (i < 180) totalPremium += premiumPerDay - ((coverType != null && coverType == CoverType.Yacht) ? premiumPerDay * 0.05m : premiumPerDay * 0.02m);
-            if (i < 365) totalPremium += premiumPerDay - ((coverType != null && coverType == CoverType.Yacht) ? premiumPerDay * 0.03m : premiumPerDay * 0.01m);
+            if (i >= 30 && i < 180) totalPremium += premiumPerDay - ((coverType != null && coverType == CoverType.Yacht) ? premiumPerDay * 0.05m : premiumPerDay * 0.02m);
+            if (i >= 180 && i < 365) totalPremium += premiumPerDay - ((coverType != null && coverType == CoverType.Yacht) ? premiumPerDay * 0.03m : premiumPerDay * 0.01m);
         }
 
         return totalPremium;
